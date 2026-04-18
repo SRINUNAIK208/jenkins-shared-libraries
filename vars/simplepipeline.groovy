41f5a616658c077460fecd37bf2efb7660bac5d4
SRINUNAIK208 <srinaik208@gmail>
2026-04-18 20:39:26 +0530

    shared-library

---
+++ vars/simplepipeline.groovy
@@ -0,0 +1,47 @@
+def call(Map configMap){
+   pipeline {
+    agent {
+        label "AGENT-1"
+    }
+    environment {
+        appVersion = ''
+        REGION = 'us-east-1'
+        ACCOUNT_ID = '824333137275'
+        PROJECT = 'roboshop'
+        COMPONENT = 'catalogue'
+    }
+    options {
+        timeout(time: 30, unit: 'MINUTES')
+        disableConcurrentBuilds()
+    }
+    parameters{
+        booleanParam(name: 'deploy', defaultValue: false, description: 'Toggle this value')
+    }
+
+    
+          stage ('UNIT TESTING') {
+            steps {
+                script {
+                    sh """
+                       echo "unit testing"
+                    """
+                }
+            }
+        }
+      
+
+       
+   
+        stage('Build') {
+            steps{
+                script {
+                  sh """
+                     echo "hello"
+                  """
+                }
+            }
+        }
+     }
+}
+
+}
